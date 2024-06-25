6a8b9d5b9b5318322f800eab209b8945b78dfd46
test-nell <156769298+test-Nel@users.noreply.github.com>
2024-06-25 16:42:34 +0900

    test

--- final_project/src/main/java/com/spring/app/HomeController.java
+++ final_project/src/main/java/com/spring/app/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
+//	test
 //@Controller
 public class HomeController {
 	
