086111f499870bcd534bfc72bd3ce27e1fde3ad7
KIM <152974341+1up0@users.noreply.github.com>
2024-06-28 17:44:52 +0900

    20240628
    업데이트

--- KEDAI/src/main/java/com/spring/app/approval/controller/ApprovalController.java
+++ KEDAI/src/main/java/com/spring/app/approval/controller/ApprovalController.java
@@ -1,8 +1,41 @@
 package com.spring.app.approval.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller 
 public class ApprovalController {
+	
+	@GetMapping(value = "/approval.kedai")
+	public ModelAndView approval(ModelAndView mav) {
+		mav.setViewName("tiles1/approval/main.tiles");
+		// /WEB-INF/views/tiles/tiles1/content/approval/main.jsp
+	//	/WEB-INF/views/tiles/tiles1/content/approval/main.tiles.jsp 페이지를 만들어야 한다.
+		return mav;
+	}
+	
+	@GetMapping(value = "/approval/newdoc.kedai")
+	public ModelAndView mom(ModelAndView mav, HttpServletRequest request ) {
+		
+		String doc_type = request.getParameter("doc_type");
+		System.out.println(doc_type);
+		
+		if(doc_type.equals("newdayoff")) {
+			mav.setViewName("tiles1/approval/newdayoff.tiles");
+		}
+		else if(doc_type.equals("newmeeting")){
+			mav.setViewName("tiles1/approval/newmeeting.tiles");
+		}
+		
+	//	/WEB-INF/views/tiles/tiles1/content/approval/newdoc.jsp 페이지를 만들어야 한다.
+		return mav;
+	}
+	
+ //  	<definition name="*/*/*/*.tiles" extends="layout-tiles">
+//  	<put-attribute name="content" value="/WEB-INF/views/tiles/{1}/content/{2}/{3}/{4}.jsp"/>
+//	</definition>
 
 }
---
+++ KEDAI/src/main/webapp/WEB-INF/views/tiles/tiles1/content/approval/newmeeting.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    
+<table>
+	<tr>
+		<th>문서번호</th>
+		
+	</tr>
+</table>
+회의록~~
+<body>
+
+</body>
+</html>
